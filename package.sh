af6076c4a085b6d7525a04ae73a11afcd9cd4a92
Juan Carlos <juancarlospaco@gmail.com>
2019-05-22 03:57:48 -0300

    Add Bash script helpers

--- package.sh
+++ package.sh
@@ -3,13 +3,13 @@ rm --verbose --force dist/*.zip
 
 python3 setup.py --verbose sdist --formats=zip
 
-rm --verbose --force --recursive faster_than_requests.egg-info/
+rm --verbose --force --recursive faster_than_csv.egg-info/
 
 rm --verbose --force *.c
 
 rm --verbose --force nimbase.h
 
 
-# pip3 uninstall faster_than_requests --yes
-# pip3 install dist/faster_than_requests-0.5.zip --no-binary :all:
-# python -c "import faster_than_requests"
+# pip3 uninstall faster_than_csv --yes
+# pip3 install dist/faster_than_csv-0.5.zip --no-binary :all:
+# python -c "import faster_than_csv"
