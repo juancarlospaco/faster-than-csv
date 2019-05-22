769fc3087158bec875da15ba4f8c0e25fb5c357c
Juan Carlos <juancarlospaco@gmail.com>
2019-05-22 03:56:37 -0300

    Update setup

--- setup.py
+++ setup.py
@@ -3,44 +3,22 @@
 
 from setuptools import Extension
 from setuptools import setup
+from os import listdir
 
 
-sources = """faster_than_csv_faster_than_csv.c
-nimpy_py_utils.c
-stdlib_dynlib.c
-stdlib_macros.c
-stdlib_parsecsv.c
-stdlib_streams.c
-stdlib_tables.c
-nimpy_nimpy.c
-stdlib_algorithm.c
-stdlib_hashes.c
-stdlib_math.c
-stdlib_parsejson.c
-stdlib_strscans.c
-stdlib_typetraits.c
-nimpy_py_lib.c
-stdlib_bitops.c
-stdlib_json.c
-stdlib_options.c
-stdlib_parseutils.c
-stdlib_strutils.c
-stdlib_unicode.c
-nimpy_py_types.c
-stdlib_complex.c
-stdlib_lexbase.c
-stdlib_ospaths.c
-stdlib_sequtils.c
-stdlib_system.c""".splitlines()
+sources = []
+for c_source_file in listdir():
+    if c_source_file.endswith(".c"):
+        sources.append(c_source_file)
 
 
 setup(
     ext_modules = [
         Extension(
-            name               = "faster_than_csv",
+            name               = "faster_than_requests",
             sources            = sources,
             extra_compile_args = ["-flto", "-ffast-math", "-march=native", "-O3"],
-            extra_link_args    = ["-flto", "-ffast-math", "-march=native", "-O3"],
+            extra_link_args    = ["-flto", "-ffast-math", "-march=native", "-O3", "-s"],
             include_dirs       = ["."],
         )
     ]
