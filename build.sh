06c69407c6841d425a4fff1d39bf45ea31135adc
Juan Carlos <juancarlospaco@gmail.com>
2019-05-22 04:15:56 -0300

    Update bash script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nim compileToC --compileOnly -d:release -d:ssl --app:lib --opt:speed --gc:markAndSweep --nimcache:. --out:faster_than_requests.so src/faster_than_requests.nim
+nim compileToC --compileOnly -d:release -d:ssl --app:lib --opt:speed --gc:markAndSweep --nimcache:. --out:faster_than_csv.so src/faster_than_csv.nim
 
 cp --verbose --force ~/.choosenim/toolchains/nim-0.19.6/lib/nimbase.h nimbase.h
 
-rm --verbose --force faster_than_requests.json
+rm --verbose --force faster_than_csv.json
