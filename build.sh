3ae29d88c943c519e166c991fd909e299adce9b9
juancarlospaco <juancarlospaco@gmail.com>
2019-12-14 01:49:25 -0300

    Fix #6

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nim compileToC --compileOnly -d:release -d:danger --app:lib --opt:speed --gc:markAndSweep --listFullPaths:off --excessiveStackTrace:off --nimcache:. --out:faster_than_csv.so src/faster_than_csv.nim
 
-cp --verbose --force ~/.choosenim/toolchains/nim-0.20.2/lib/nimbase.h nimbase.h
+cp --verbose --force ~/.choosenim/toolchains/nim-1.0.4/lib/nimbase.h nimbase.h
 
 rm --verbose --force faster_than_csv.json
