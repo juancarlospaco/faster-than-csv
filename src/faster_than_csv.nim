c1a558cdf1306294c90dc053f38821135fab3bbc
Juan Carlos <JuanCarlospaco@gmail.com>
2020-06-05 19:48:16 -0300

    details

--- src/faster_than_csv.nim
+++ src/faster_than_csv.nim
@@ -6,7 +6,7 @@ const html_table_header = """<!DOCTYPE html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.0/css/bulma.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.2/css/bulma.min.css">
 </head>
 <body><br><br>
   <div class="container is-fluid">
