f8cb1c9260ba92835ee99e50190c1a4afc1e94d9
Sekou Diao <52342703+SekouDiaoNlp@users.noreply.github.com>
2021-05-09 00:19:37 +0200

    Update package4pypi.sh
    changed directories to map

--- package4pypi.sh
+++ package4pypi.sh
@@ -1,5 +1,5 @@
 rm --verbose --force dist/*.zip
-rm --verbose --force --recursive dist/faster_than_csv/faster_than_csv/
-rm --verbose --force --recursive dist/faster_than_csv/faster_than_csv/__pycache__/
-cp --verbose --recursive faster_than_csv dist/faster_than_csv/
+rm --verbose --force --recursive dist/faster_than_csv/
+rm --verbose --force --recursive dist/faster_than_csv/__pycache__/
+cp --verbose --recursive faster_than_csv dist/
 cd dist && zip -9 -T -v -r faster_than_csv.zip *
