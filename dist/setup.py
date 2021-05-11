b47fa8d2ca45f150ef08365d1f2bca04c8f8d885
Juan Carlos <juancarlospaco@gmail.com>
2021-05-11 15:19:39 -0300

    Improve package for PyPI, so it let me upload it

---
+++ dist/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+  name         = "choosenim_install",
+  author       = "Juan_Carlos.nim",
+  author_email = "UNKNOWN",
+  url          = "UNKNOWN",
+  package_data = {"": ["*.nim", "*.nims", "*.cfg", "*.dll", "*.so", "*.h", "*.hpp"]},
+)
