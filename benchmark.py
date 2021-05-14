42e0629810a50e540b779dde390b9076f4da418f
Juan Carlos <juancarlospaco@gmail.com>
2021-05-14 20:11:05 -0300

    Performance improvements

--- benchmark.py
+++ benchmark.py
@@ -45,7 +45,7 @@ def run_test(library, filename, repetitions, setup_test, run_test, timer=None):
     return result
 
 
-def run_all_benchmarks(filename='', repetitions=10_000, output_file="results.csv", **kwargs):
+def run_all_benchmarks(filename='', repetitions=1_000, output_file="results.csv", **kwargs):
     results = []
     tests = []
     timer_type = kwargs.get('timer')
@@ -82,7 +82,7 @@ def plot_benchmark_results(in_path="results.csv", output_path="results_graph.png
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Benchmarks for CSV libs")
     parser.add_argument('--filename', metavar='u', type=str, default='sample.csv', help="filename")
-    parser.add_argument('--repetitions', metavar='c', type=int, default=10_000, help="Repetitions")
+    parser.add_argument('--repetitions', metavar='c', type=int, default=1_000, help="Repetitions")
     parser.add_argument('--timer', type=str, default="real", choices=('real','cpu'), help="Timer type: real [default] or cpu")
     args = vars(parser.parse_args())
     assert args.get('filename') is not None, "filename must not be an empty string."
